6db917caeb193eb2f1fa8cf3925ad7d39e32dbb4
vasudev-io <ee20947@bristol.ac.uk>
2024-01-29 01:02:28 +0000

    [Refactor] Dashboard display to include Sensitivity scores

--- frontend/src/services/DataFetchServiceAPI.ts
+++ frontend/src/services/DataFetchServiceAPI.ts
@@ -1,5 +1,9 @@
 export interface DataFetchServiceAPI {
-  getClassifications(): Promise<{ [classname: string]: { correct: number, incorrect: number }}>;
+  getClassifications(): Promise<{ [classname: string]: { correct: number, incorrect: number } }>;
   getStatistics(): Promise<{ accuracy: number; error: number; samples: number; } | Error>;
   getConfusionMatrix(): Promise<{ [actualLabel: string]: { [predictedLabel: string]: number } }>;
+  getSensitivityScores(): Promise<{ [classname: string]: number }>;
+  
 }
+
+
